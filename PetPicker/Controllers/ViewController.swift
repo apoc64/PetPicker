d51f88ecf8ca12fe27aacc15944cce69eeefa1ff
apoc64 <sschwedt@gmail.com>
2018-10-03 16:00:37 -0600

    Segues to swipe screen when logged in

--- PetPicker/Controllers/ViewController.swift
+++ PetPicker/Controllers/ViewController.swift
@@ -26,13 +26,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         let defaults = UserDefaults.standard
         
         let id = defaults.integer(forKey: "user_id")
         print(id)
-        // Do any additional setup after loading the view, typically from a nib.
+        if id > 0 {
+            print("I should segue")
+            self.performSegue(withIdentifier: "successfulLogin", sender: nil)
+        }
     }
 
-
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
 }
 
