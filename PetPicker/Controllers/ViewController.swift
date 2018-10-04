9b06e66e703938fcfe08c363b4eb77c8316e41f2
apoc64 <sschwedt@gmail.com>
2018-10-04 17:45:02 -0600

    Creates user model object on login and passes it to swipe vc

--- PetPicker/Controllers/ViewController.swift
+++ PetPicker/Controllers/ViewController.swift
@@ -10,39 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var loggedInUser: User!
     @IBOutlet weak var userName: UITextField!
     @IBOutlet weak var password: UITextField!
     
     @IBAction func login(_ sender: UIButton) {
-        
         if let name = userName.text, let pass = password.text {
             let pp = PPApi(sendingVC: self)
             pp.login(name: name, password: pass)
         }
     }
     
-    func loginSegue() {
+    func loginSegue(user: User) {
+        loggedInUser = user
         performSegue(withIdentifier: "successfulLogin", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        let vc = segue.destination as! SwipeViewController
+        // Pass the selected object to the new view controller.
+        vc.currentUser = loggedInUser
     }
     
     override func viewDidAppear(_ animated: Bool) {
         // check to see if logged in already
         let defaults = UserDefaults.standard
-        
         let id = defaults.integer(forKey: "user_id")
-        print(id)
+        // get actual user object
+        let user = User(data: ["id": id])
+        print(user)
         if id > 0 {
             print("I should segue")
-            self.loginSegue()
+            self.loginSegue(user: user)
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destination.
-//        // Pass the selected object to the new view controller.
-//    }
 }
 
--- PetPicker/Models/User.swift
+++ PetPicker/Models/User.swift
@@ -9,5 +9,25 @@
 import Foundation
 
 class User {
+    var id: Int!
+    var name: String!
+    var key: String? // is null in seeds?
+    var speciesToAdopt: String!
+    var description: String!
+    var pic: String!
     
+    init(data: [String: Any]) {
+        id = data["id"] as? Int
+        name = data["name"] as? String
+        key = data["key"] as? String
+        speciesToAdopt = data["species_to_adopt"] as? String
+        description = data["dascription"] as? String
+        pic = data["pic"] as? String
+    }
+    
+    func setAsDefault() {
+        // store key?
+        let defaults = UserDefaults.standard
+        defaults.set(id, forKey: "user_id")
+    }
 }
